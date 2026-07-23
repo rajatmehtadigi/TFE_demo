3d53ead0330abd5646e75aedce9ff2530d73acff
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-07-23 18:45:02 +0530

    Updated instance size and also updated index.html file content for testing purpose

--- user_data.sh
+++ user_data.sh
@@ -9,12 +9,13 @@ cat <<'EOF' > /var/www/html/index.html
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Terraform Enterprise Demo by Rajat Mehta</title>
+    <title>Terraform Enterprise Demo</title>
 </head>
 <body>
     <h1>Hello from Terraform Enterprise!</h1>
     <p>This EC2 instance was created using Terraform.</p>
     <p>Infrastructure as Code is powerful.</p>
+    <h3>This Demo is created by Rajat Mehta</h3>
 </body>
 </html>
 EOF
--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "public_subnet_cidr" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.medium"
 }
 
 variable "allowed_ssh_cidr" {
